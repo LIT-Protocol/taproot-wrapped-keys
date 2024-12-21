01d2fb561ae92e93f3dc60a677bad9a723b50b1c
Ansh <anshsaxena4190@gmail.com>
2024-12-21 20:44:02 +0530

    checkpoint

--- esbuild.js
+++ esbuild.js
@@ -4,15 +4,12 @@ import { build } from "esbuild";
 build({
     entryPoints: ["./src/actions/taproot-action.js"],
     bundle: true,
-    format: "iife", // This ensures everything is bundled into a self-contained file
-    globalName: "LitAction", // Gives a name to the IIFE
     minify: false,
     sourcemap: false,
     outfile: "./actions/bundled-action.js",
-    platform: "browser", // Change to browser since we're running in Lit's environment
-    target: ["es2020"],
+    platform: "node",
     metafile: true,
-    external: ["ethers"], // Keep ethers external since it's provided by Lit
+    external: ["ethers"],
     define: {
         'global': 'globalThis',
         'require': 'undefined' // This prevents require calls in the bundled output
@@ -29,14 +26,3 @@ build({
         },
     }],
 });
-
-// esbuild-shims.js
-globalThis.Buffer = globalThis.Buffer || (() => {
-    const { Buffer } = require('buffer/');
-    return Buffer;
-})();
-
-// Add any other necessary global shims here
-globalThis.process = globalThis.process || {
-    env: { NODE_ENV: 'production' }
-};
